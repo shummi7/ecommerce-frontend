17c2713dbcaaf320f17dccd99176d8d06b40d3a8
shummi7 <sharmila16moorthi@gmail.com>
2023-03-14 15:44:41 -0500

    Updates

---
+++ precache-manifest.77e33628653f97b41bc6316ba616a6ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd31f419debf501b6c90e2c3bce3d73",
+    "url": "/ecommerce-frontend/index.html"
+  },
+  {
+    "revision": "1dda00eaf24e6a1c77c4",
+    "url": "/ecommerce-frontend/static/css/main.1ef48d77.chunk.css"
+  },
+  {
+    "revision": "fbce90df5df2c0f5f589",
+    "url": "/ecommerce-frontend/static/js/2.28e7aee0.chunk.js"
+  },
+  {
+    "revision": "1dda00eaf24e6a1c77c4",
+    "url": "/ecommerce-frontend/static/js/main.399d942d.chunk.js"
+  },
+  {
+    "revision": "64092d5b3f23c1977365",
+    "url": "/ecommerce-frontend/static/js/runtime~main.49c246c1.js"
+  }
+]);
